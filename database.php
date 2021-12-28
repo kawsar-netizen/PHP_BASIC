6e93413994466419f633886b11f3ee191663efcd
kawsar-netizen <60083776+kawsar-netizen@users.noreply.github.com>
2021-12-29 01:27:12 +0800

    Add files via upload
    Database Connection for php and mysql

---
+++ database.php
@@ -0,0 +1,9 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername   = "root";
+$dbPassword   = "";
+$dbName       = "php_database_connection";
+$conn         = mysqli_conncet($dbServername,$dbUsername,$dbPassword,$dbName);
+
+?>
